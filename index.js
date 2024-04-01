a7d5e9f2a7cfdf71924ab66a048ab2e4ec419fa0
Aarthi5680 <113370942+Aarthi5680@users.noreply.github.com>
2024-04-01 12:33:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+//==//
+
+let a= 20;
+if(a==20){
+    console.log(true)
+}else{
+    console.log(false)
+}
+
+//===//
+
+let b='35';
+
+if(b===35){
+    console.log(true)
+}else{
+    console.log(false)
+}
+
+//===//
+
+let c=35;
+
+if(c===35){
+    console.log(true)
+}else{
+    console.log(false)
+}
+
+
+//for looping statement//
+var x=20;
+
+for(i=0;i<20;i++){
+    console.log(i)
+
+}
+
+
